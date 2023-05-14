a6d677703719ac7fd8809eb400a4eae7447a4854
ilyaRozin <rozinila5@gmail.com>
2023-05-14 15:11:00 +0300

    correct pins

--- main.py
+++ main.py
@@ -29,15 +29,13 @@ class MainBoard:
     def paint_board(self):
         self.image = Image.new("RGB", (self.width, self.height), (0, 255, 0))
 
-    def append_new_element(self, x_c, y_c, h, w):
-        self.elements.append(BoardElement(x_c, y_c, h, w))
+    def append_new_element(self, x_c, y_c, h, w, name):
+        self.elements.append(BoardElement(x_c, y_c, h, w, self.gridDivisionSize, name))
 
     def append_element(self, instance, x_c, y_c):
-        instance.grid_size = self.gridDivisionSize
-        instance.x_c = x_c
-        instance.y_c = y_c
-        instance.append_pins(instance.spm)
-        self.elements.append(instance)
+        element_buf = BoardElement(x_c, y_c, instance.h, instance.w, self.gridDivisionSize, instance.element_name)
+        element_buf.append_pins(instance.spm)
+        self.elements.append(element_buf)
 
     def show_board(self):
         self.paint_elements()
@@ -59,13 +57,15 @@ class BoardElement:
     pin = []        #Пины к которым идет
     grid_size = 0   #Размер сетки
     spm = []
+    element_name = ""
 
-    def __init__(self, x, y, h, w, grid_size):
+    def __init__(self, x, y, h, w, grid_size, name):
         self.x_c = x
         self.y_c = y
         self.h = h
         self.w = w
         self.grid_size = grid_size
+        self.element_name = name
 
     def paint_element(self, image, grid_size):
         image_print_element = ImageDraw.Draw(image)
@@ -80,28 +80,37 @@ class BoardElement:
 
     def append_pins(self, locations_places):
         for i in locations_places:
-            self.append_new_pin(i[0], i[1], i[2])
+           if len(i) != 0:
+               self.append_new_pin(i[0], i[1], i[2], i[3])
 
-    def append_new_pin(self, location, place_pin, connect_pins):
+    def append_new_pin(self, location, place_pin, connect_pins, step_size_contact):
+        if step_size_contact == 1:
+            h_new = math.floor(self.h*self.grid_size/(2*self.grid_size))
+            w_new = math.floor(self.w*self.grid_size/(2*self.grid_size))
+        else:
+            indent_h = (self.h - (len(place_pin)-1) * step_size_contact) / 2
+            indent_w = (self.w - (len(place_pin) - 1) * step_size_contact) / 2
+            h_new = self.h/2 - indent_h
+            w_new = self.w/2 - indent_h
         if len(place_pin) != 0:
             if location == "right":
                 for i in place_pin:
                     x_pin = self.x_c + self.w/2 - 0.25
-                    y_pin = self.y_c - math.ceil((self.h/2)/self.grid_size) + (i-1) - 0.225
+                    y_pin = self.y_c - h_new + (i-1)*step_size_contact - 0.225
                     self.pin.append(Pin(x_pin, y_pin, connect_pins))
             elif location == "left":
                 for i in place_pin:
                     x_pin = self.x_c - self.w/2 - 0.25
-                    y_pin = self.y_c - math.ceil((self.h/2)/self.grid_size) + (i-1) - 0.225
+                    y_pin = self.y_c - h_new + (i-1)*step_size_contact - 0.225
                     self.pin.append(Pin(x_pin, y_pin, connect_pins))
             elif location == "up":
                 for i in place_pin:
-                    x_pin = self.x_c - math.ceil(self.w/2/self.grid_size) + (i-1) - 0.225
+                    x_pin = self.x_c - w_new + (i-1)*step_size_contact - 0.225
                     y_pin = self.y_c - self.h/2 - 0.25
                     self.pin.append(Pin(x_pin, y_pin, connect_pins))
             elif location == "down":
                 for i in place_pin:
-                    x_pin = self.x_c - math.ceil(self.w/2/self.grid_size) + (i-1) - 0.225
+                    x_pin = self.x_c - w_new + (i-1)*step_size_contact - 0.225
                     y_pin = self.y_c + self.h / 2 - 0.25
                     self.pin.append(Pin(x_pin, y_pin, connect_pins))
             else:
@@ -125,38 +134,60 @@ class Pin:
 
 
 def init_dictionary():
-    element_dictionary: dict[str, BoardElement] = {"resistor10Om": BoardElement(0, 0, 11, 4.5, 1),
-                                                   "resistor1kOm": BoardElement(0, 0, 11, 4.5, 1),
-                                                   "resistor100Om": BoardElement(0, 0, 11, 4.5, 1),
-                                                   "resistor1Om": BoardElement(0, 0, 11, 4, 1),
-                                                   "capacitor10mkF": BoardElement(0, 0, 4.5, 3.2, 1),
-                                                   "capacitor100mkF": BoardElement(0, 0, 3.5, 2.8, 1),
-                                                   "capacitor1mkF": BoardElement(0, 0, 2, 1.25, 1),
-                                                   "inductance220mkG": BoardElement,
-                                                   "inductance10mkG": BoardElement,
-                                                   "K176TM2": BoardElement,
-                                                   "TPS51200DRCR": BoardElement,
-                                                   "CS48505S": BoardElement,
-                                                   "ADM3070EARZ": BoardElement,
-                                                   "AD790JNZ": BoardElement,
-                                                   "CD74HC123E": BoardElement,
-                                                   "23LC1024-I/SN": BoardElement,
-                                                   "ADUC812BSZ": BoardElement,
-                                                   "AD420ARZ-32-REEL": BoardElement}
+    element_dictionary: dict[str, BoardElement] = {"resistor10Om": BoardElement(0, 0, 11, 4.5, 1, "resistor10Om"),
+                                                   "resistor1kOm": BoardElement(0, 0, 11, 4.5, 1, "resistor1kOm"),
+                                                   "resistor100Om": BoardElement(0, 0, 11, 4.5, 1, "resistor100Om"),
+                                                   "resistor1Om": BoardElement(0, 0, 11, 4, 1, "resistor1Om"),
+                                                   "capacitor10mkF": BoardElement(0, 0, 4.5, 3.2, 1, "capacitor10mkF"),
+                                                   "capacitor100mkF": BoardElement(0, 0, 3.5, 2.8, 1, "capacitor100mkF"),
+                                                   "capacitor1mkF": BoardElement(0, 0, 2, 1.25, 1, "capacitor1mkF"),
+                                                   "inductance220mkG": BoardElement(0, 0, 12, 7.5, 1, "inductance220mkG"),
+                                                   "inductance10mkG": BoardElement(0, 0, 3.2, 1.8, 1, "inductance10mkG"),
+                                                   "K176TM2": BoardElement(0, 0, 19.5, 6.6, 1, "K176TM2"),
+                                                   "TPS51200DRCR": BoardElement(0, 0, 3, 3, 1, "TPS51200DRCR"),
+                                                   "CS48505S": BoardElement(0, 0, 4.9, 3.9, 1, "CS48505S"),
+                                                   "ADM3070EARZ": BoardElement(0, 0, 8.5, 6, 1, "ADM3070EARZ"),
+                                                   "AD790JNZ": BoardElement(0, 0, 9.4, 6.2, 1, "AD790JNZ"), #
+                                                   "CD74HC123E": BoardElement(0, 0, 19.45, 6.6, 1, "CD74HC123E"),
+                                                   "23LC1024-I/SN": BoardElement(0, 0, 4.9, 3.9, 1, "23LC1024-I/SN"),
+                                                   "ADUC812BSZ": BoardElement(0, 0, 16, 16, 1, "ADUC812BSZ"),
+                                                   "AD420ARZ-32-REEL": BoardElement(0, 0, 15.2, 7.6, 1, "AD420ARZ-32-REEL")}
 
-    element_dictionary["resistor1Om"].spm = [["left", [1, 2, 3], []], ["right", [1], []], ["up", [1], []], ["down", [1], []]]
-    element_dictionary["resistor10Om"].spm = [["left", [1, 2, 3], []], ["right", [1], []], ["up", [], []], ["down", [], []]]
-    element_dictionary["resistor100Om"].spm = [["left", [1], []], ["right", [1], []], ["up", [], []], ["down", [], []]]
-    element_dictionary["resistor1kOm"].spm = [["left", [1], []], ["right", [1], []], ["up", [], []], ["down", [], []]]
-    element_dictionary["capacitor10mkF"].spm = [["left", [1], []], ["right", [1], []], ["up", [], []], ["down", [], []]]
-    element_dictionary["capacitor1mkF"].spm = [["left", [1], []], ["right", [1], []], ["up", [], []], ["down", [], []]]
-    element_dictionary["capacitor100mkF"].spm = [["left", [1], []], ["right", [1], []], ["up", [], []], ["down", [], []]]
+    element_dictionary["resistor1Om"].spm = [[], [], ["up", [3], [], 1], ["down", [3], [], 1]]
+    element_dictionary["resistor10Om"].spm = [[], [], ["up", [3], [], 1], ["down", [3], []], 1]
+    element_dictionary["resistor100Om"].spm = [[], [], ["up", [3], [], 1], ["down", [3], [], 1]]
+    element_dictionary["resistor1kOm"].spm = [[], [], ["up", [3], [], 1], ["down", [3], [], 1]]
+    element_dictionary["capacitor10mkF"].spm = [[], [], ["up", [3], [], 1], ["down", [3], [], 1]]
+    element_dictionary["capacitor1mkF"].spm = [[], [], ["up", [3], [], 1], ["down", [3], [], 1]]
+    element_dictionary["capacitor100mkF"].spm = [[], [], ["up", [2], [], 1], ["down", [2], [], 1]]
+    element_dictionary["inductance220mkG"].spm = [[], [], ["up", [3], [], 1], ["down", [3], [], 1]]
+    element_dictionary["inductance10mkG"].spm = [[], [], ["up", [1], [], 1], ["down", [1], [], 1]]
+    element_dictionary["K176TM2"].spm = [["left", [1, 2, 3, 4, 5, 6, 7], [], 2.5],
+                                         ["right", [1, 2, 3, 4, 5, 6, 7], [], 2.5],
+                                         ["up", [], [], 2.5], ["down", [], [], 2.5]]
+    element_dictionary["TPS51200DRCR"].spm = [["left", [1, 2, 3, 4, 5], [], 0.5],
+                                              ["right", [1, 2, 3, 4, 5], [], 0.5], [], []]
+    element_dictionary["CS48505S"].spm = [["left", [1, 2, 3, 4], [], 1.27], ["right", [1, 2, 3, 4], [], 1.27], [], []]
+    element_dictionary["ADM3070EARZ"].spm = [["left", [1, 2, 3, 4, 5, 6, 7], [], 1.27],
+                                             ["right", [1, 2, 3, 4, 5, 6, 7], [], 1.27], [], []]
+    element_dictionary["AD790JNZ"].spm = [["left", [1, 2, 3, 4], [], 2.54], ["right", [1, 2, 3, 4], [], 2.54], [], []]
+    element_dictionary["CD74HC123E"].spm = [["left", [1, 2, 3, 4, 5, 6, 7, 8], [], 2.54],
+                                            ["right", [1, 2, 3, 4, 5, 6, 7, 8], [], 2.54], [], []]
+    element_dictionary["23LC1024-I/SN"].spm = [["left", [1, 2, 3, 4], [], 1.27],
+                                               ["right", [1, 2, 3, 4], [], 1.27], [], []]
+    element_dictionary["ADUC812BSZ"].spm = [["left", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [], 0.9],
+                                            ["right", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [], 0.9],
+                                            ["up", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [], 0.9],
+                                            ["down", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [], 0.9]]
+    element_dictionary["AD420ARZ-32-REEL"].spm = [["left", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [], 1.27],
+                                            ["right", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [], 1.27], [], []]
     return element_dictionary
 
 
 if __name__ == '__main__':
     new_board = MainBoard(1000, 1000, 8)
     el_dict = init_dictionary()
-    new_board.append_element(el_dict["resistor10Om"], 10, 11)
-    new_board.append_element(el_dict["capacitor100mkF"], 20, 20)
+    new_board.append_element(el_dict["ADUC812BSZ"], 80, 80)
+    # Разобраться с поворотами
+    # Начать что-то делать с проводами
     new_board.show_board()
